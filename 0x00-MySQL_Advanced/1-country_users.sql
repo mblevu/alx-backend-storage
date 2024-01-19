60031a2cddbe10ab54f141b3489c134e801dccd5
mblevu <brianislevu@gmail.com>
2024-01-19 12:37:57 +0300

    Fix typo in table creation comment

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,4 +1,4 @@
---creates a table users gracefully
+-- creates a table users gracefully
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
