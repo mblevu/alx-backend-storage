3a554b31bc22d0b8b89299640f87238c93fdf7f9
mblevu <brianislevu@gmail.com>
2024-01-19 13:34:38 +0300

    Refactor store.sql trigger to use item name instead of ID

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,11 +1,5 @@
 --  creates a trigger that decreases the quantity of an item after adding a new order
-DELIMITER //
-CREATE TRIGGER decrease_quantity
-AFTER INSERT ON orders
+CREATE TRIGGER buy_trigger
+AFTER INSERT on orders
 FOR EACH ROW
-BEGIN
-    UPDATE items
-    SET quantity = quantity - NEW.quantity
-    WHERE id = NEW.item_id;
-END; //
-DELIMITER ;
+UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
