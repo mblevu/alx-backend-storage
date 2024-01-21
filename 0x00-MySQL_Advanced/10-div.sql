0727518960bae736fd0e3be8dc2a18a613796e2c
mblevu <brianislevu@gmail.com>
2024-01-21 05:51:44 +0300

    Fix SafeDiv function to handle division by zero

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,15 +1,14 @@
 -- creates a function SafeDiv that divides (and returns) the first
 -- by the second number or returns 0 if the second number is equal to 0.
-DROP FUNCTION if EXISTS SafeDiv;
+DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER $$
-CREATE FUNCTION SafeDiv(a INT, secbond INT)
-RETURNS FLOAT DETERMINISTICS
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
 BEGIN
-    DECLARE result FLOAT DEFAULT 0;
-
-    IF b != 0 THEN
-        SET result = a / b
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
     END IF;
-    RETURN result;
 END $$
 DELIMITER ;
