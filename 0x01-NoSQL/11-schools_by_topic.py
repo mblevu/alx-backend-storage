b41db3ae4a4c4483a0c54ca6cba9fed831422182
mblevu <brianislevu@gmail.com>
2024-01-23 23:48:42 +0300

    Add schools_by_topic function to retrieve schools with a specific topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""returns the list of school having a specific topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """returns the list of school having a specific topic"""
+    specific_topics = mongo_collection.find({"topics": topic})
+    return specific_topics
